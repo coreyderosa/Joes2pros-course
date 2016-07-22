a85b2c30a7e4421f5455fda22703f182e89b6c8b
coreyderosa <coreyderosa@gmail.com>
2016-07-21 19:50:03 -0600

    Creating table values
    create 5 new movies with info

---
+++ SQLQuery8.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM tblMovie
+
+INSERT INTO tblMovie
+VALUES (1, 'AList Explorers', 96, 'PG-13')
+
+INSERT INTO tblMovie
+VALUES (2, 'Bonker Bonzo', 75, 'G')
+
+INSERT INTO tblMovie
+VALUES (3, 'Chumps to Champs', 75, 'PG-13')
+
+INSERT INTO tblMovie
+VALUES (4, 'Dare or Die', 110, 'R'),
+	(5, 'EeeeGhads', 88, 'G')
