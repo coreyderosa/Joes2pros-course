cb2f45eafe8849b44266fee51e22dd1b742797cf
coreyderosa <coreyderosa@gmail.com>
2016-07-24 15:19:41 -0600

    Combined create table with create values in table
    Combined create table code with create values in table code

--- SQLQuery8.sql
+++ SQLQuery8.sql
@@ -1,5 +1,24 @@
-SELECT * 
-FROM tblMovie
+USE master
+GO
+
+--Run only if dB exists
+IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'dbMovie')
+DROP DATABASE dbMovie
+GO
+
+CREATE DATABASE dbMovie
+GO
+
+USE dbMovie
+GO
+
+CREATE TABLE tblMovie
+(
+m_id int PRIMARY KEY NOT NULL,
+m_title varchar(30) NOT NULL,
+m_runtime int NULL,
+m_Rating varchar(10) NULL
+)
 
 INSERT INTO tblMovie
 VALUES (1, 'AList Explorers', 96, 'PG-13')
