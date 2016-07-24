9e4aa1337aa029cc0aa4050d12ad34c45d1599b7
coreyderosa <coreyderosa@gmail.com>
2016-07-24 17:53:37 -0600

    Create db and table with values
    Creating a movie db

---
+++ SQLQuery19.sql
@@ -0,0 +1,29 @@
+USE master
+GO
+
+--Run only if dB exists
+IF EXISTS (SELECT * FROM sys.databases WHERE [name] = 'dbMovie')
+DROP DATABASE dbMovie
+GO
+
+CREATE DATABASE dbMovie
+GO
+
+USE dbMovie
+GO
+
+CREATE TABLE tblMovie
+(
+m_id int PRIMARY KEY NOT NULL,
+m_title varchar(30) NOT NULL,
+m_runtime int NULL,
+m_Rating varchar(10) NULL
+)
+GO
+
+INSERT INTO tblMovie
+VALUES (1, 'AList Explorers', 96, 'PG-13'),
+(2, 'Bonker Bonzo', 75, 'G'),
+(3, 'Chumps to Champs', 75, 'PG-13'),
+(4, 'Dare or Die', 110, 'R'),
+(5, 'EeeeGhads', 88, 'G')
