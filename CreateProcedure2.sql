8f52ee534d0e08e30f44e05bc573fe6eae6205f0
coreyderosa <coreyderosa@gmail.com>
2016-07-26 19:36:59 -0600

    Create Procedure 2
    Select all Non WA employees

---
+++ CreateProcedure2.sql
@@ -0,0 +1,8 @@
+CREATE PROC GetNonWashingtonEmployees
+AS
+SELECT E.Firstname, E.lastname, L.city, L.[state]
+FROM Employee AS E INNER JOIN Location AS L
+ON E.LocationID = L.LocationID
+WHERE L.[state]  != 'WA'
+
+EXEC GetNonWashingtonEmployees
